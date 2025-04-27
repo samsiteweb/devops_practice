53dbf542af9fad1299ed98cc5b81837cf9e1418b
samuellawalo <lawalsamuelajamu@gmail.com>
2025-04-27 19:53:44 -0300

    ci test

--- SemanixBackend/src/Services/RequestManagement/RequestManagement.Api/Program.cs
+++ SemanixBackend/src/Services/RequestManagement/RequestManagement.Api/Program.cs
@@ -14,7 +14,7 @@ namespace RequestManagement.Api;
 
 public class Program
 {
-    //test build update 2
+    //test build update 1
     public static void Main(string[] args)
     {
         using var logger = new LoggerConfiguration()
