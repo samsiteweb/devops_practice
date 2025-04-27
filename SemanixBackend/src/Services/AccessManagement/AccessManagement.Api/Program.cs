1cc05c8687972a6aa0e9742a30fd198a27d5e88d
samuellawalo <lawalsamuelajamu@gmail.com>
2025-04-27 05:49:21 -0300

    Update Program.cs

--- SemanixBackend/src/Services/AccessManagement/AccessManagement.Api/Program.cs
+++ SemanixBackend/src/Services/AccessManagement/AccessManagement.Api/Program.cs
@@ -14,7 +14,7 @@ namespace AccessManagement.Api;
 
 public class Program
 {
-    //test build update 1
+    //test build update 
     public static void Main(string[] args)
     {
         using var logger = new LoggerConfiguration()
