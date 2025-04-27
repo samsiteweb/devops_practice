37913b9f6ac793a3df5c1a83c9bb4556b6bee01c
samuellawalo <lawalsamuelajamu@gmail.com>
2025-04-27 05:52:43 -0300

    updated helm version

--- SemanixBackend/src/Services/AccessManagement/AccessManagement.Api/Program.cs
+++ SemanixBackend/src/Services/AccessManagement/AccessManagement.Api/Program.cs
@@ -14,7 +14,7 @@ namespace AccessManagement.Api;
 
 public class Program
 {
-    //test build update 
+    //test build updat
     public static void Main(string[] args)
     {
         using var logger = new LoggerConfiguration()
