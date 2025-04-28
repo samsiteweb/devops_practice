ec33448e0744fea0c7c0177eb5896bab68b9e540
samuellawalo <lawalsamuelajamu@gmail.com>
2025-04-28 01:21:22 -0300

    updated

--- SemanixBackend/src/Services/AccessManagement/AccessManagement.Api/Program.cs
+++ SemanixBackend/src/Services/AccessManagement/AccessManagement.Api/Program.cs
@@ -14,7 +14,7 @@ namespace AccessManagement.Api;
 
 public class Program
 {
-    //test build update 8
+    //test build update 0
     public static void Main(string[] args)
     {
         using var logger = new LoggerConfiguration()
--- SemanixBackend/src/Services/RequestManagement/RequestManagement.Api/Program.cs
+++ SemanixBackend/src/Services/RequestManagement/RequestManagement.Api/Program.cs
@@ -14,7 +14,7 @@ namespace RequestManagement.Api;
 
 public class Program
 {
-    //test build update 7
+    //test build update 9
     public static void Main(string[] args)
     {
         using var logger = new LoggerConfiguration()
